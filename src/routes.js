388d517ea47c1268806c63fcb61ee14c661fc802
GowtamSagarP <42111393+GowtamSagarP@users.noreply.github.com>
2019-03-01 20:02:14 +0530

    Update Router
    Added Path to Profile

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import Profile from './screens/profile/Profile';
 import Login from "./screens/login/Login";
 import Home from './screens/home/Home';
 import ApiClient from "./client/ApiClient";
