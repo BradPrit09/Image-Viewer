3e68cb3c85b22bbef661e21369d1b8305fcf4976
GowtamSagarP <42111393+GowtamSagarP@users.noreply.github.com>
2019-03-01 20:00:30 +0530

    Update Router.js
    Added Route to Profile

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,7 @@ class Routes extends Component {
 				<div className="main-container">
 					<Route exact path='/' render={(props) => <Login {...props} />} />
 					<Route exact path='/home' render={(props) => <Home apiClient={apiClient} {...props} />} />
+				        <Route exact path='/profile' render={(props) => <Profile {...props} />} />
 					
 				</div>
 			</Router>
